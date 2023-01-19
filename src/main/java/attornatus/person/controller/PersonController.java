50100edc2ab5d59840dadcb5c588ae05b8f32977
aynsantos <allyson.santos@live.com>
2023-01-19 14:31:31 -0300

    Project created

---
+++ src/main/java/attornatus/person/controller/PersonController.java
@@ -0,0 +1,16 @@
+package attornatus.person.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class PersonController {
+
+    @GetMapping
+    public String Hello(){
+        return "Hello API";
+    }
+}
---
+++ src/test/java/attornatus/person/PersonApiApplicationTests.java
@@ -0,0 +1,13 @@
+package attornatus.person;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
